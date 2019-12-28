50dc1f37986e84306ca918370fff4d655854bfe3
Christoph Ehlers <Christoph.Ehlers1988@gmx.de>
2019-12-28 22:58:19 +0100

    set AuthPlugin-Name

--- src/serverplugin/AuthPlugin.ts
+++ src/serverplugin/AuthPlugin.ts
@@ -2,6 +2,10 @@ import { serverevent } from '../core/enums';
 import Serverplugin from '../core/Serverplugin';
 
 export default class extends Serverplugin {
+  constructor() {
+    super();
+    this.setName('AuthPlugin');
+  }
   public getListenEvents(): serverevent[] {
     return [serverevent.clientWillConnect];
   }
