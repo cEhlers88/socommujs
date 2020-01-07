87ba6d2d02741c0785a3ac4d4e56819b62817f7d
Christoph Ehlers <Christoph.Ehlers1988@gmx.de>
2020-01-07 20:21:04 +0100

    [#15] - added more required methods to serverplugin-interface

--- src/interfaces/serverplugin.ts
+++ src/interfaces/serverplugin.ts
@@ -2,6 +2,10 @@ import {EServerEvent} from "../lib/enums";
 
 export default interface IServerplugin{
     getListenEvents(): EServerEvent[] | string,
+    getRequiredServerData(): string[],
     handleEvent(event: EServerEvent, eventProps?: unknown): void,
-    run(data?: unknown): void
+    run(data?: unknown): void,
+    init():void,
+    setData(key: string, value: any): IServerplugin,
+    setLogHandle(newHandle: CallableFunction): IServerplugin
 }
